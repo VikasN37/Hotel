b62803410b04b02768bd0916961012a908de2618
Vikas <vikas630602@gmail.com>
2024-10-05 17:25:50 +0530

    website build

---
+++ app/_components/Counter.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Counter({ users }) {
+  const [count, setCount] = useState(0);
+  console.log(users);
+  return <h1>counter</h1>;
+}
